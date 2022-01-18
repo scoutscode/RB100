3f9fd31060b4d92937fdc5cea3dde045cd080559
Chris Dang <chrisdang408@gmail.com>
2022-01-18 12:09:15 -0500

    Basics fifth exercise

---
+++ TheBasics/TheBasics5.rb
@@ -0,0 +1,9 @@
+factorial_five = 5*4*3*2
+factorial_six = 6*5*4*3*2
+factorial_seven = 7*6*5*4*3*2
+factorial_eight = 8*7*6*5*4*3*2
+
+puts "factorial of 5 is: #{factorial_five}"
+puts "factorial of 6 is: #{factorial_six}"
+puts "factorial of 7 is: #{factorial_seven}"
+puts "factorial of 8 is: #{factorial_eight}"
