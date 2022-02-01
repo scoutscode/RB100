731d14ca6edd6f3eda0b9dfc0f7c9631a2ea987e
Chris Dang <chrisdang408@gmail.com>
2022-02-01 18:29:21 -0500

    Arrays Exercise 7

---
+++ Arrays/Arrays7.rb
@@ -0,0 +1,11 @@
+#use each_with_index method to iterate through an array of your creation that prints each index and value of the array 
+
+top_five_games = ["mario brothers",
+                  "excite bike",
+                  "ring king",
+                  "castlevania",
+                  "double dragon"]
+
+top_five_games.each_with_index do | game, index |
+  puts "#{index + 1}. #{game}"
+end
